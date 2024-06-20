d960f4fdc4c6e8974283c6d17839c4b5fb3cec43
Amruth <ammumandappa@gmail.com>
2024-06-20 22:58:47 +0530

    view component

--- HelloWorld/app/index.tsx
+++ HelloWorld/app/index.tsx
@@ -1,17 +1,51 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+
+import React from "react";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "black"
-        
+        backgroundColor: "black",
       }}
     >
-      <Text style={{color:'white'}}>namaste World</Text>
+      <View
+        style={{
+          backgroundColor: "lightblue",
+          width: 200,
+          height: 200,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "lightgreen",
+          width: 200,
+          height: 200,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></View>
     </View>
   );
 }
+
+// import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//         backgroundColor: "black"
+
+//       }}
+//     >
+//       <Text style={{color:'white'}}>hello World</Text>
+//     </View>
+//   );
+// }
