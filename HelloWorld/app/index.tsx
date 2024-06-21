a2052fa22d8a4ca6eaea97a007d44c5f94481f25
Amruth <ammumandappa@gmail.com>
2024-06-21 16:11:55 +0530

    modal

--- HelloWorld/app/index.tsx
+++ HelloWorld/app/index.tsx
@@ -1,9 +1,20 @@
-import { View, Text, Image, ImageBackground, ScrollView, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Pressable,
+  Modal,
+} from "react-native";
+import { useState } from "react";
 const logoImg = require("@/assets/images/partial-react-logo.png");
 
 import React from "react";
 
 export default function Index() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View
       style={{
@@ -11,6 +22,7 @@ export default function Index() {
         backgroundColor: "black",
         justifyContent: "center",
         alignItems: "center",
+        padding: 40,
       }}
     >
       <ScrollView>
@@ -18,76 +30,39 @@ export default function Index() {
           <Text style={{ color: "pink" }}>lorem87</Text> World
         </Text>
 
-        <Image
-          source={{ uri: "https://picsum.photos/200/300" }}
-          style={{ width: 300, height: 300 }}
-        ></Image>
-        <Button title="press"></Button>
-        <Text style={{ color: "white" }}>
-          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
-          and typesetting industry. Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum. assages, and more
-          recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum. Why do we use it? It is a long
-          established fact that a reader will be distracted by the readable
-          content of a page when looking at its layout. The point of using Lorem
-          Ipsum is that it has a more-or-less normal distribution of letters, as
-          opposed to using 'Content here, content here', making it look like
-          readable English. Many desktop publishing packages and web page
-          editors now use Lorem Ipsum as their default model text, and a search
-          for 'lorem ipsum' will uncover many web sites still in their infancy.
-          Various versions have evolved over the years, sometimes by accident,
-          sometimes on purpose (injected humour and the like). Where does it
-          come from? Contrary to popular belief, Lorem Ipsum is not simply
-          random text. It has roots in a piece of classical Latin literature
-          from 45 BC, making it over 2000 years old. Richard McClintock, a Latin
-          professor at Hampden-Sydney College in Virginia, looked up one of the
-          more obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-          going through the cites of the word in classical literature,
-          discovered the undoubtable source. Lorem Ipsum comes from sections
-          1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-          of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
-          on the theory of ethics, very popular during the Renaissance. The
-          first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
-          frassages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum. Why do we use it?
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like). Where
-          does it come from? Contrary to popular belief, Lorem Ipsum is not
-          simply random text. It has roots in a piece of classical Latin
-          literature from 45 BC, making it over 2000 years old. Richard
-          McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-          looked up one of the more obscure Latin words, consectetur, from a
-          Lorem Ipsum passage, and going through the cites of the word in
-          classical literature, discovered the undoubtable source. Lorem Ipsum
-          comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
-          Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-          This book is a treatise on the theory of ethics, very popular during
-          the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-          amet..", comes from a line in section 1.10.32. The standard chunk of
-          Lorem Ipsum used since the 1500s is reproduced below for those
-          interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
-          Malorum" by Cicero are also reproduced in their exact original form,
-          accompanied by English versions from the 1914 translation by H.
-          Rackham.om a line in section 1.10.32. The standard chunk of Lorem
-          Ipsum used since the 1500s is reproduced below for those interested.
-          Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by
-          Cicero are also reproduced in their exact original form, accompanied
-          by English versions from the 1914 translation by H. Rackham.
-        </Text>
+        <Pressable onPress={() => console.log("Image Pressed")}>
+          <Image
+            source={{ uri: "https://picsum.photos/200/300" }}
+            style={{ width: 300, height: 300 }}
+          ></Image>
+        </Pressable>
+        <Button
+          title="press"
+          onPress={() => setIsModalVisible(true)}
+          color="midnightblue"
+        ></Button>
+        <Pressable onPress={() => console.log("Text Pressed")}>
+          <Text style={{ color: "white" }}>
+            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem eb sites still in their infancy. going through the
+            cites of the word in classical literature, disetimes on purpose
+            (injected humour and the like). Where does it come from? Contrary to
+            popular belief, Lorem Ipsum is not simply random text. It has roots
+            in a piece of classical Latin literature from 45 BC, making it over
+            2000 years old. Richard McClintock, a Latin professor at
+            Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, 1.10.32 and 1.10.33 from "de
+            Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+            exact original form, accompanied by English versions from the 1914
+            translation by H. Rackham.
+          </Text>
+        </Pressable>
 
         {/* <Image source={logoImg} style={{ width: 300, height: 300 }} /> */}
         <Image
@@ -99,6 +74,21 @@ export default function Index() {
         <Text style={{ color: "red"}}>Image Text</Text>
       </ImageBackground> */}
       </ScrollView>
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)} // if we click on back button
+        animationType="fade"
+        presentationStyle="fullScreen"  //on;ly affects IOS and not android 
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 20 }}>
+          <Text>Modal Content</Text>
+          <Button
+            title="close"
+            color="midnightblue"
+            onPress={() => setIsModalVisible(false)}
+          ></Button>
+        </View>
+      </Modal>
     </View>
   );
 }
