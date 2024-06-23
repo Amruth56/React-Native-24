e96ced0b565b6ac63b8fe32ed6f734fab45fb4d4
Amruth <ammumandappa@gmail.com>
2024-06-23 16:36:06 +0530

    flex box

--- HelloWorld/app/index.tsx
+++ HelloWorld/app/index.tsx
@@ -10,21 +10,38 @@ import {
   StatusBar,
   ActivityIndicator,
   Alert,
-  StyleSheet
+  StyleSheet,
 } from "react-native";
 import { useState } from "react";
 import App1 from "@/components/coreComponents";
 import Greet from "@/components/greet";
 import React from "react";
-import Styling from '@/stylingRN/inLine'
-import StyleApi from '@/stylingRN/styleSheetApi'
+import Styling from "@/stylingRN/inLine";
+import StyleApi from "@/stylingRN/styleSheetApi";
+import Box from "@/RNlayout/box";
 
 export default function Index() {
- return(
-  // <App1></App1>
+  return (
+    // <App1></App1>
 
-  // <Styling></Styling>
-  <StyleApi></StyleApi>
-
- )
+    // <Styling></Styling>
+    // <StyleApi></StyleApi>
+    <View style={styles.container}>
+      <Box style={{ backgroundColor: "brown" }}>Box1</Box>
+      <Box style={{ backgroundColor: "black" }}>Box2</Box>
+      <Box style={{ backgroundColor: "violet" }}>Box3</Box>
+      <Box style={{ backgroundColor: "pink" }}>Box4</Box>
+      <Box style={{ backgroundColor: "green" }}>Box5</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box6</Box>
+      <Box style={{ backgroundColor: "indigo" }}>Box7</Box>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "black",
+  },
+});
