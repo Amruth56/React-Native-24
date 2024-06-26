5d12842f8b7131a87eb7690bf1ce12c3fc2ac310
Amruth <ammumandappa@gmail.com>
2024-06-26 15:26:00 +0530

    .

--- DynamicUI/app/index.tsx
+++ DynamicUI/app/index.tsx
@@ -1,35 +1,33 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <View style = {styles.box}>
-      <Text style = {styles.text}>hi
-        .</Text>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>hi .</Text>
+      </View>
     </View>
   );
 }
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:"plum",
+    backgroundColor: "plum",
     alignItems: "center",
     justifyContent: "center",
   },
-  text:{
-    fontSize:34
+  text: {
+    fontSize: windowWidth>500? 34 : 24,
   },
-  box:{
-    width:300,
-    height:300,
+  box: {
+    width: windowWidth>500 ? '70%' : '90%',
+    height: windowHeight>600 ? '60%' : '80%',
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor:"lightblue",
-    borderRadius:30
-
-  }
-})
+    backgroundColor: "lightblue",
+    borderRadius: 30,
+  },
+});
