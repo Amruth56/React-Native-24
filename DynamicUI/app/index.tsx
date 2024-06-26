b7d191af9c3d094960afc18cae0326560b830cc1
Amruth <ammumandappa@gmail.com>
2024-06-26 18:52:38 +0530

    .

--- DynamicUI/app/index.tsx
+++ DynamicUI/app/index.tsx
@@ -1,16 +1,35 @@
 import { Text, View, StyleSheet, Dimensions } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function Index() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get('window')
+  })
+
+  useEffect(()=> {
+    const subscription = Dimensions.addEventListener("change", ({window}) => {
+      setDimensions({window})
+    })
+    return () => subscription?.remove();
+  })
+
+  const {window} = dimensions
+  const windowWidth = window.width
+  const windowHeight = window.height
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>hi .</Text>
+      <View style={[styles.box, {
+        width: windowWidth > 500 ? '70%':'90%',
+        height: windowHeight >600 ? "60%" : "90%",
+      }]}>
+        <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>hi .</Text>
       </View>
     </View>
   );
 }
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
@@ -19,12 +38,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontSize: windowWidth>500? 34 : 24,
-  },
+  // text: {
+  //   fontSize: windowWidth>500? 34 : 24,
+  // },
   box: {
-    width: windowWidth>500 ? '70%' : '90%',
-    height: windowHeight>600 ? '60%' : '80%',
+    // width: windowWidth>500 ? '70%' : '90%',
+    // height: windowHeight>600 ? '60%' : '80%',
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "lightblue",
