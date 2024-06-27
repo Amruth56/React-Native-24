fc43925b1772239dc4da6095c96ef9ed8580c9ab
Amruth <ammumandappa@gmail.com>
2024-06-27 16:21:05 +0530

    .

--- DynamicUI/app/index.tsx
+++ DynamicUI/app/index.tsx
@@ -5,6 +5,7 @@ import {
   Dimensions,
   useWindowDimensions,
   SafeAreaView,
+  Platform,
 } from "react-native";
 
 export default function Index() {
@@ -23,7 +24,7 @@ export default function Index() {
             },
           ]}
         >
-          <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>hi .</Text>
+          <Text style={styles.text}>hi .</Text>
         </View>
       </View>
     </SafeAreaView>
@@ -38,19 +39,32 @@ const styles = StyleSheet.create({
    },
   container: {
     flex: 1,
-    backgroundColor: "plum",
+    backgroundColor: Platform.OS === 'android' ? "black" :"plum",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: Platform.OS === "android"?25:0,
   },
-  // text: {
-  //   fontSize: windowWidth>500? 34 : 24,
-  // },
+  text: {
+    ...Platform.select({
+      ios:{
+        color:"purple",
+        fontSize: 24,
+        fontStyle: "italic"
+      },
+      android:{
+        color:"midnightblue",
+        fontSize: 34,
+      }
+    }),
+    fontWeight:"bold",
+    textAlign: "center",
+  },
   box: {
     // width: windowWidth>500 ? '70%' : '90%',
     // height: windowHeight>600 ? '60%' : '80%',
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "lightblue",
+    backgroundColor: "yellow",
     borderRadius: 30,
   },
 });
