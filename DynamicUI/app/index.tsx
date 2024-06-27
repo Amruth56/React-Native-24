9030c49060ae9501ebeed209cfabf274a943f801
Amruth <ammumandappa@gmail.com>
2024-06-27 15:15:32 +0530

    safe area view

--- DynamicUI/app/index.tsx
+++ DynamicUI/app/index.tsx
@@ -1,25 +1,41 @@
-import { Text, View, StyleSheet, Dimensions, useWindowDimensions } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  useWindowDimensions,
+  SafeAreaView,
+} from "react-native";
 
 export default function Index() {
- 
-  const windowWidth = useWindowDimensions().width
-  const windowHeight = useWindowDimensions().height
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.box, {
-        width: windowWidth > 500 ? '70%':'90%',
-        height: windowHeight >600 ? "60%" : "90%",
-      }]}>
-        <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>hi .</Text>
+    <SafeAreaView style = {styles.safeContainer}>
+      <View style={styles.container}>
+        <View
+          style={[
+            styles.box,
+            {
+              width: windowWidth > 500 ? "30%" : "90%",
+              height: windowHeight > 600 ? "30%" : "90%",
+            },
+          ]}
+        >
+          <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>hi .</Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 // const windowWidth = Dimensions.get("window").width;
 // const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
+  safeContainer: { flex: 1, 
+    backgroundColor:"plum"
+   },
   container: {
     flex: 1,
     backgroundColor: "plum",
