488e130bf4b67ec2478d2a009b864e5538da6840
Amruth <ammumandappa@gmail.com>
2024-06-27 20:20:49 +0530

    .

---
+++ ProjectOne/app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>hi.</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor:"#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text:{
+  }
+})
