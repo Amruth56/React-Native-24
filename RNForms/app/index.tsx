9ef9ee7382a4dc97ca3f45d5e6c38d0f6e16436c
Amruth <ammumandappa@gmail.com>
2024-07-01 19:08:49 +0530

    .

--- RNForms/app/index.tsx
+++ RNForms/app/index.tsx
@@ -1,15 +1,63 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <Text style={styles.label}>UserName</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+        ></TextInput>
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value ={password}
+          onChangeText={setPassword}
+        ></TextInput>
+        <Button title="login" onPress={() => {}}></Button>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "f5f5f5",
+  },
+  form: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 40,
+    marginBottom: 10,
+    padding: 5,
+  },
+});
