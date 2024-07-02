d41c80c7938e045317de08d33bf970b2026c5a70
Amruth <ammumandappa@gmail.com>
2024-07-02 13:01:13 +0530

    .

--- RNNetworking/app/index.tsx
+++ RNNetworking/app/index.tsx
@@ -5,13 +5,19 @@ import {
   SafeAreaView,
   StatusBar,
   FlatList,
-  ActivityIndicator
+  ActivityIndicator,
+  TextInput,
+  Button,
 } from "react-native";
 import { useState, useEffect } from "react";
 
 export default function Index() {
   const [postList, setPostList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const [refresh, setRefresh] = useState(false);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(
@@ -20,43 +26,85 @@ export default function Index() {
     //convert response to json
     const data = await response.json();
     setPostList(data);
-    setIsLoading(false)
+    setIsLoading(false);
+  };
+
+  const handelRefresh = () => {
+    setRefresh(true);
+    fetchData(20);
+    setRefresh(false);
+  };
+
+  const addPost = async () => {
+    setIsPosting(true);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: postTitle, body: postBody }),
+    });
+    const newPost = await response.json()
+    setPostList([newPost, ...postList]);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  if(isLoading){
-    return(
-      <SafeAreaView style = {styles.loadingContainer}>
-        <ActivityIndicator size = 'large' color = '0000ff'></ActivityIndicator>
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="0000ff"></ActivityIndicator>
         <Text>LoAdInG...</Text>
       </SafeAreaView>
-    )
+    );
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={postList}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.titleText}>{item.title}</Text>
-                <Text style={styles.bodyText}>{item.body}</Text>
-              </View>
-            );
-          }}
-          ItemSeparatorComponent={() => <View style={{ height: 16 }}></View>}
-          ListEmptyComponent={<Text>No post available</Text>}
-          ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
-          ListFooterComponent={
-            <Text style={styles.footerText}>End of list</Text>
-          }
-        ></FlatList>
-      </View>
+      <>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Post title"
+            value={postTitle}
+            onChangeText={setPostTitle}
+          ></TextInput>
+          <TextInput
+            style={styles.input}
+            placeholder="Post body"
+            value={postBody}
+            onChangeText={setPostBody}
+          ></TextInput>
+          <Button
+            title={isPosting ? "Adding..." : "Add Post"}
+            onPress={addPost}
+            disabled={isPosting}
+          ></Button>
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={postList}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.card}>
+                  <Text style={styles.titleText}>{item.title}</Text>
+                  <Text style={styles.bodyText}>{item.body}</Text>
+                </View>
+              );
+            }}
+            ItemSeparatorComponent={() => <View style={{ height: 16 }}></View>}
+            ListEmptyComponent={<Text>No post available</Text>}
+            ListHeaderComponent={
+              <Text style={styles.headerText}>Post List</Text>
+            }
+            ListFooterComponent={
+              <Text style={styles.footerText}>End of list</Text>
+            }
+            refreshing={refresh}
+            onRefresh={handelRefresh}
+          ></FlatList>
+        </View>
+      </>
     </SafeAreaView>
   );
 }
@@ -99,11 +147,26 @@ const styles = StyleSheet.create({
     marginTop: 12,
     marginBottom: 12,
   },
-  loadingContainer:{
-    flex:1,
-    backgroundColor:'f5f5f5',
-    paddingTop:StatusBar.currentHeight,
-    justifyContent:"center",
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "f5f5f5",
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
     alignItems: "center",
-  }
+  },
+  inputContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "black",
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
+  },
 });
